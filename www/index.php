dd20008d1cf35f67019ac05ac79bb36e0f1e1b92
enricoschumann <enricoschumann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-23 19:05:42 +0000

    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/nmof@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<img src = "http://enricoschumann.wikidot.com/local--files/table-of-contents/book.jpg"/>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
