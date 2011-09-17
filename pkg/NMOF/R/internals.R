a720c1a1ad61f9ea874cc5fc20a5ad408dd64f7b
enricoschumann <enricoschumann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-17 18:54:51 +0000

    internals.R added
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/nmof@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/NMOF/R/internals.R
@@ -0,0 +1,35 @@
+##
+makeInteger <- function(x, label, min = 1L) {
+    x <- as.integer(x)
+    if (is.na(x) || x < min)
+        stop(label, "must be an integer greater/equal to ", min)
+    x
+}
+
+##
+anyNA <- function(x)
+    if (!is.null(x) && !is.function(x)) any(is.na(x)) else FALSE
+
+##
+checkList <- function(passedList, defaultList, label = "'algo'") {
+    ## NAs in list
+    if (any(sapply(passedList,anyNA)))
+        stop("NAs are not allowed in list ", label)
+
+    ## unnamed elements in list
+    if ("" %in% names(passedList))
+        warning("'algo' contained unnamed elements",
+                call. = FALSE)
+
+    ## ununsed elements in list
+    unusedOptions <- setdiff(names(passedList), names(defaultList))
+    unusedOptions <- setdiff(unusedOptions, "")
+    if (length(unusedOptions))
+        warning("unknown names in 'algo': ",
+                paste(unusedOptions, collapse = ", "),
+                call. = FALSE)
+}
+
+##
+mRU <- function(m, n) array(runif(m*n), dim = c(m,n))
+mRN <- function(m, n) array(rnorm(m*n), dim = c(m,n))
