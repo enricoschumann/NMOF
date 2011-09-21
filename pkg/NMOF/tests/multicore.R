7498d9a57aae771f914fa8ce7bfb70839d84cbdc
enricoschumann <enricoschumann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-21 07:56:48 +0000

    multicore.R added
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/nmof@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/NMOF/tests/multicore.R
@@ -0,0 +1,59 @@
+## tests for 'multicore': _not_ run
+
+runMC <- FALSE
+
+if (runMC) {
+    require("NMOF")
+    if (!require("multicore"))
+        stop("package 'multicore' not available")
+    if (!require("RUnit"))
+        stop("package 'RUnit' not available")
+
+
+    ## gridSearch ##
+    testFun  <- function(x) {
+        Sys.sleep(0.1)
+        x[1L] + x[2L]^2
+    }
+    lower <- 1:3; upper <- 5; n <- 3
+    system.time(sol1 <- gridSearch(fun = testFun,
+                                   lower = lower, upper = upper,
+                                   n = n, printDetail = FALSE))
+    system.time(sol2 <- gridSearch(fun = testFun,
+                                   lower = lower, upper = upper,
+                                   n = n, printDetail = FALSE,
+                                   multicore = TRUE))
+    checkEquals(sol1, sol2)
+    checkEquals(sol2$minlevels, 1:3)
+
+
+    ## bracketing ##
+    testFun <- function(x,k) {
+        Sys.sleep(0.1)
+        cos(1/x^2)
+    }
+    system.time(bracketing(testFun, interval = c(0.3, 0.9),
+                           n = 10L))
+    system.time(bracketing(testFun, interval = c(0.3, 0.9),
+                           n = 10L, method = "multicore"))
+
+
+    ## restartOpt ##
+    xTRUE <- runif(5L)
+    data <- list(xTRUE = xTRUE, step = 0.02)
+    OF <- function(x, data)
+        max(abs(x - data$xTRUE))
+    neighbour <- function(x, data)
+        x + runif(length(data$xTRUE))*data$step - data$step/2
+    x0 <- runif(5L)
+    algo <- list(q = 0.05, nS = 50L, nT = 5L,
+                 neighbour = neighbour, x0 = x0,
+                 printBar = FALSE, printDetail = FALSE)
+    sols1 <- restartOpt(fun = TAopt, n = 10L,
+                       OF = OF, algo = algo, data = data)
+    checkEquals(length(sols1), 10L)
+    sols2 <- restartOpt(fun = TAopt, n = 10L,
+                       OF = OF, algo = algo, data = data,
+                        multicore = TRUE)
+    checkEquals(length(sols2), 10L)
+}
