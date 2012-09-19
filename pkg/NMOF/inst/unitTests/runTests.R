2dfeb4d59a264457c4959f7c44030aa1e24cee2c
enricoschumann <enricoschumann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-19 20:15:34 +0000

    v0.26-0
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/nmof@147 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/NMOF/inst/unitTests/runTests.R
+++ pkg/NMOF/inst/unitTests/runTests.R
@@ -2,7 +2,7 @@ localTesting <- TRUE
 require("NMOF")
 if (require("RUnit", quietly = TRUE)) {
     if (localTesting)
-        path <- "c:/packages/NMOF/NMOF/pkg/NMOF/inst/unitTests" else
+        path <- "~/Packages/NMOF/NMOF/pkg/NMOF/inst/unitTests" else
     path <- system.file("unitTests", package = "NMOF")
 
     myTestSuite <- defineTestSuite("NMOF",
