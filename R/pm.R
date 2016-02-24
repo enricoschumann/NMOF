28c48e79214af3025eaae9fa597fe365bc113509
Enrico Schumann <es@enricoschumann.net>
2016-02-24 15:07:29 +0100

    removed comments/TODOs

--- R/pm.R
+++ R/pm.R
@@ -1,12 +1,7 @@
 ## -*- truncate-lines: t; -*-
-## Time-stamp: <2013-09-30 16:13:53 CEST (es)>
 
 pm <- function(x, xp = 2, threshold = 0, lower = TRUE,
                normalise = FALSE, na.rm = FALSE) {
-
-    ## TODO: check normalise: only if xp is integer?
-    ## is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)
-    ##     abs(x - round(x)) < tol
 
     if (length(d <- dim(x))) {
         n <- d[1L]
@@ -44,10 +39,6 @@ pm <- function(x, xp = 2, threshold = 0, lower = TRUE,
 cm <- function(x, xp = 2, threshold = 0, lower = TRUE,
                normalise = FALSE, na.rm = FALSE) {
 
-    ## TODO: check normalise: only if xp is integer?
-    ## is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)
-    ##     abs(x - round(x)) < tol
-
     if (length(d <- dim(x))) {
         n <- d[1L]
         sum <- colSums
@@ -72,12 +63,10 @@ cm <- function(x, xp = 2, threshold = 0, lower = TRUE,
     else 
         sum(x^xp, na.rm = na.rm)/2^xp
     ans <- ans/n * sum(x > threshold)/n
+
     if (normalise)
         ans^(1/xp)
     else
         ans
 
 }
-## x <- rnorm(20)
-## cm(x)/0.6
-## pm(x)
