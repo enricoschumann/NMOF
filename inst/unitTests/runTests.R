cbef4b7d3dd7fdee1027863faa43fcb77184931a
Enrico Schumann <es@enricoschumann.net>
2016-04-12 09:33:28 +0200

    inst/unitTests/runTests.R: fixed -- 'testFileRegexp' was wrong and so unit test were not run

--- inst/unitTests/runTests.R
+++ inst/unitTests/runTests.R
@@ -3,17 +3,17 @@ if (tolower(Sys.getenv("ES_PACKAGE_TESTING")) == "true" &&
     require("RUnit", quietly = TRUE) &&
     require(pkg, quietly = TRUE, character.only = TRUE)) {
 
-    path <- paste0("~/Packages/", pkg, "/inst/unitTests")
+    path <- paste0("~/Packages/", pkg, "/inst/unitTests/")
 
     ## if the package is installed and you want to run
     ## the tests, use this path:    
     ##
     ##     path <- system.file("unitTests", package = "NMOF")
     ##
-    
+
     myTestSuite <- defineTestSuite(pkg,
                                    dirs = path,
-                                   testFileRegexp = "ut_.*")
+                                   testFileRegexp = "^unitTests.*")
     stopifnot(isValidTestSuite(myTestSuite))
     testResult <- runTestSuite(myTestSuite, verbose = 0L)
     printTextProtocol(testResult,
