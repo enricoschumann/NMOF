5aa2de3e33a6b6773199e77218502fefe1796a72
Enrico Schumann <es@enricoschumann.net>
2016-09-30 07:25:25 +0200

    Add function 'xtContractValue'

---
+++ R/xtContractValue.R
@@ -0,0 +1,18 @@
+## -*- truncate-lines: t; fill-column: 65; comment-column: 50; -*-
+
+xtContractValue <- function(quoted.price, coupon, do.round = TRUE) {
+
+    do.round <- if (do.round)
+                    round
+                else
+                    function(x, ignore)
+                        x
+
+    yield <- 100 - quoted.price
+    i <- yield/200
+    v <- do.round(1/(1+i), 8)
+    n <- 20
+    c <- coupon/2
+
+    1000*(do.round(c*(1-v^20)/i,8) + 100*do.round(v^20,8))
+}
