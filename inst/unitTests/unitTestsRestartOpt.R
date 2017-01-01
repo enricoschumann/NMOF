146013c780531d51bf02f5c10f9ba803a2f07003
Enrico Schumann <es@enricoschumann.net>
2017-01-01 11:55:38 +0100

    Small adjustment to unit tests

--- inst/unitTests/unitTestsRestartOpt.R
+++ inst/unitTests/unitTestsRestartOpt.R
@@ -1,22 +1,32 @@
 test.restartOpt <- function() {
     testParallel <- FALSE
 
-    xTRUE <- runif(5L)
-    data <- list(xTRUE = xTRUE, step = 0.02)
+    xTRUE <- rep(0, 5)
+    data <- list(xTRUE = xTRUE)
     OF <- function(x, data)
         max(abs(x - data$xTRUE))
     neighbour <- function(x, data)
-        x + runif(length(data$xTRUE))*data$step - data$step/2
+        rep(0, 5)
 
     x0 <- runif(5L)
-    algo <- list(q = 0.05, nS = 5L, nT = 5L,
+    algo <- list(q = 0.01, nS = 1L, nT = 5L,
                  neighbour = neighbour, x0 = x0,
                  printBar = FALSE, printDetail = FALSE)
 
-        sols <- restartOpt(fun = TAopt, n = 5L,
-                           OF = OF, algo = algo, data = data)
+    sols <- restartOpt(fun = TAopt, n = 5L,
+                       OF = OF, algo = algo, data = data)
     checkEquals(length(sols), 5L)
 
+    ## w <- getOption("warn")
+    ## options(warn = 2)
+    ## checkException(sols <- restartOpt(
+    ##                    fun = TAopt, n = 5L,
+    ##                    OF = OF, algo = algo, data = data,
+    ##                    best.only = TRUE), silent = TRUE)
+    ## options(warn = w)
+
+
+    
     ## tests for snow/multicore: slow!
     if (testParallel) {
         OF <- function(x, data) {
