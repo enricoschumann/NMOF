da277878f169f994916c439098e97ee3e6de9c18
Enrico Schumann <es@enricoschumann.net>
2017-01-09 20:46:15 +0100

    Add new function CPPI

---
+++ R/CPPIgap.R
@@ -0,0 +1,37 @@
+## -*- truncate-lines: t; fill-column: 65; comment-column: 50; -*-
+
+CPPI <- function(S, multiplier, floor, r, tau = 1, gap = 1) {
+    T <- length(S) - 1  ## number of periods: 1, ..., T
+   dt <- tau/T          ## timestep
+    m <- multiplier     
+    B <-                ## bond investment
+    C <-                ## cushion
+    V <-                ## value
+    E <-                ## exposure
+    N <- numeric(T + 1) ## units of risky asset
+
+    F <- floor*exp(r*-tau*(1 - 0:T/T))
+    zero <- 1
+    V[zero] <- 1
+    C[zero] <- V[zero] - F[zero]
+    E[zero] <- m * C[zero]
+    N[zero] <- E[zero]/S[zero]
+    B[zero] <- V[zero] - E[zero]
+    for (t in 1:T + 1) {
+
+        B[t] <- B[t - 1] * exp(r*dt)
+        V[t] <- N[t - 1] * S[t] + B[t]
+        C[t] <- max(0, V[t] - F[t])
+        
+        if (t %% gap == 0L) {
+            E[t] <- min(m * C[t], V[t])
+            N[t] <- E[t]/S[t]
+            B[t] <- V[t] - E[t]            
+        } else {
+            E[t] <- V[t] - B[t]
+            N[t] <- N[t - 1]
+        }
+    }
+    list(V = V, C = C, B = B, F = F, E = E, N = N, S = S)
+}
+
