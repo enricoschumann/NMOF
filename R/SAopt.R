e0e047dacb7c975b008e442cadbb8e80790a0ea2
Enrico Schumann <es@enricoschumann.net>
2017-01-09 20:48:00 +0100

    Add outline for Simulated Annealing

---
+++ R/SAopt.R
@@ -0,0 +1,270 @@
+SAopt <- function(OF, algo = list(), ...) {
+    algoD <- list(nD = 2000L, ## random steps for computing thresholds
+                  nT = 10L,   ## number of thresholds
+                  nS = 1000L, ## steps per threshold
+                  initT = 1,  ## starting quantile for thresholds
+                  alpha = 0.9,
+                  sx = 1,
+                  x0 = NULL,  ## initial solution
+                  vT = NULL,  ## threshold sequence
+                  neighbour = NULL,
+                  printDetail = TRUE,
+                  printBar = TRUE,
+                  storeF = TRUE,
+                  storeSolutions = FALSE,
+                  classify = FALSE,
+                  OF.target = NULL)
+
+    checkList(algo, algoD)
+    algoD[names(algo)] <- algo
+    if (!exists(".Random.seed", envir = .GlobalEnv,
+                inherits = FALSE))
+        state <- NA else state <- .Random.seed
+
+
+    ## user *must* specify the following
+    if (is.null(algoD$neighbour))
+        stop("specify a neighbourhood function ", sQuote("algo$neighbour"))
+    if (!is.function(algoD$neighbour))
+        stop(sQuote("algo$neighbour"), " must be a function")
+    if (is.null(algoD$x0))
+        stop("specify start solution ", sQuote("algo$x0"))
+
+    if (is.function(algoD$x0)) 
+        x0 <- algoD$x0() else x0 <- eval(algoD$x0)
+
+    OF1 <- function(x)
+        OF(x, ...)
+    N1 <- function(x)
+        algoD$neighbour(x, ...)
+
+    target.reached <- FALSE
+    T <-  algoD$initT
+    alpha <-  algoD$alpha
+    
+    printDetail <- algoD$printDetail
+    printBar <- algoD$printBar
+    if (printBar && printDetail > 1)
+        printBar <- FALSE
+    if (printDetail)
+        cat("\nThreshold Accepting.\n")
+
+
+    nT <- makeInteger(algoD$nT, "algo$nT")
+    nS <- makeInteger(algoD$nS, "algo$nS")
+    nD <- makeInteger(algoD$nD, "algo$nD")
+    stepUp <- makeInteger(algoD$stepUp, "algo$stepUp", 0L)
+    niter <- nS * nT * (stepUp+1L)
+
+    ## compute thresholds
+    if (is.null(algoD$vT)) {
+        if (algoD$q < .Machine$double.eps^0.5) {
+            vT <- numeric(nT)
+        } else {
+            if (printDetail) {
+                cat("\nComputing thresholds ... ")
+                flush.console()
+                gc(FALSE)
+                startTime <- proc.time()
+            }
+            if (printBar)
+                whatGen <- txtProgressBar(min = 1, max = nD, style = 3,
+                                          getOption("width")*0.9)
+            xc  <- x0
+            xcF <- OF1(xc)
+            diffF <- numeric(nD)
+            diffF[] <- NA
+            for (i in seq_len(nD)){
+                if (printBar)
+                    setTxtProgressBar(whatGen, value = i)
+                xn  <- N1(xc)
+                xnF <- OF1(xn)
+                diffF[i] <- abs(xcF - xnF)
+                xc  <- xn
+                xcF <- xnF
+            }
+            vT <- algoD$q * ((nT - 1L):0)/nT
+            if (any(is.na(diffF)))
+                stop("objective function evaluated to NA")
+            vT <- quantile(diffF, vT, na.rm = FALSE)
+            vT[nT] <- 0  ## set last threshold to zero
+            if (printBar)
+                close(whatGen)
+            if (printDetail) {
+                cat("OK.")
+                endTime <- proc.time()
+                cat("\nEstimated remaining running time:",
+                    as.numeric(endTime[3L] - startTime[3L]) /
+                    nD * niter * (stepUp + 1L),
+                    "secs.\n")
+                flush.console()
+            }
+        }
+    } else {
+        vT <- algoD$vT
+    }
+    vT <- vT * scale
+    nT <- length(vT)
+    niter <- nS * nT
+
+    ## evaluate initial solution
+    xc <- x0
+    xcF <- OF1(xc)
+    xbest <- xc
+    xbestF <- xcF
+
+    if (algoD$storeF) {
+        Fmat <- array(NA, dim = c(niter, 2L))
+        colnames(Fmat) <- c("xnF", "xcF")
+    } else Fmat <- NA
+    if (algoD$storeSolutions)
+        xlist <- list(xn = vector("list", length = niter),
+                      xc = vector("list", length = niter)) else xlist <- NA
+    if (printDetail) {
+        cat("\nRunning Simulated Annealing ...\n")
+        cat("Initial solution: ", prettyNum(xbestF),"\n")
+        flush.console()
+    }
+    if (printBar)
+        whatGen <- txtProgressBar(min = 1, max = niter, style = 3,
+                                  getOption("width")*0.9)
+
+    counter <- 0L
+    for (t in seq_len(nT)) {
+        for (s in seq_len(nS)) {
+            ## counter = total number of iterations
+            counter <- counter + 1L
+
+            xn <- N1(xc)
+            xnF <- OF1(xn)
+            if (xnF <= xcF || exp((xcF - xnF)/T) > runif(1L)) {
+                xc <- xn
+                xcF <- xnF
+                if (xnF <= xbestF) {
+                    xbest <- xn
+                    xbestF <- xnF
+                }
+
+            }
+            
+            if (printBar)
+                setTxtProgressBar(whatGen, value = counter)
+
+            ## store OF values
+            if (algoD$storeF) {
+                Fmat[counter, 1L] <- xnF ## proposed sol.
+                Fmat[counter, 2L] <- xcF ## accepted sol. (cummin=xbestF)
+            }
+
+            ## store solutions
+            if (algoD$storeSolutions) {
+                xlist[[c(1L, counter)]] <- xn
+                xlist[[c(2L, counter)]] <- xc
+            }
+
+            if (printDetail > 1) {
+                if (counter %% printDetail == 0L) {
+                    cat("Best solution (iteration ", counter,
+                        "/", niter, "): ",
+                        prettyNum(xbestF),"\n", sep = "")
+                    flush.console()
+                }
+            }
+            
+            ## check stopif value
+            if (!is.null(algo$OF.target) && xbestF <= algo$OF.target) {
+                if (printDetail) {
+                    cat("Target value (", prettyNum(algo$OF.target), ") ",
+                        "for objective function reached: ",
+                        prettyNum(xbestF), "\n", sep = "")
+                    flush.console()
+                    target.reached  <- TRUE
+                }
+                break    
+            }
+        }
+        if (target.reached)
+            break
+        T <- T*alpha
+    }
+    if (printDetail)
+        cat("Finished.\nBest solution overall: ",
+            prettyNum(xbestF), "\n", sep = "")
+    if (printBar)
+        close(whatGen)
+
+    ans <- list(xbest = xbest, OFvalue = xbestF,
+                Fmat = Fmat, xlist = xlist, vT = vT,
+                initial.state = state)
+    if (algoD$classify)
+        class(ans) <- "SAopt"
+    ans
+}
+
+SA.info <- function(n = 0L) {
+    e <- parent.frame(3L + n)
+    step <- NA
+    threshold <- NA
+    iteration <- NA
+    iteration.sampling <- NA
+    xbest <- NA
+    if (exists("i", envir = e, inherits = FALSE))
+        step <- get("i", envir = e, inherits = FALSE)
+    if (exists("s", envir = e, inherits = FALSE))
+        step <- get("s", envir = e, inherits = FALSE)
+    if (exists("t", envir = e, inherits = FALSE))
+        threshold <- get("t", envir = e, inherits = FALSE)
+    if (exists("counter", envir = e, inherits = FALSE))
+        iteration <- get("counter", envir = e, inherits = FALSE)
+    if (exists("xbest", envir = e, inherits = FALSE))
+        xbest <- get("xbest", envir = e, inherits = FALSE)
+    list(iteration.sampling = iteration.sampling,
+         iteration = iteration,
+         step = step,
+         threshold = threshold,
+         xbest = if (is.list(xbest)) list(xbest) else xbest)
+}
+
+
+                                        # METHODS
+
+print.SAopt <- function(x, ...) {
+    cat("Threshold Accepting\n")
+    cat("_ final objective-function value: ", x$OFvalue, "\n")
+    invisible(x)
+}
+
+plot.SAopt <- function(x, y, plot.type = "interactive", ...) {
+    if (plot.type == "interactive") {
+        dev.new(title = "SAopt: Threshold sequence")
+        defaults <- list(x    = x$vT,
+                         xlab = "Threshold",
+                         ylab = "Values",
+                         main = "SAopt: Threshold Sequence",
+                         type = "b")
+        do.call("plot", defaults)
+        if (!is.null(x$Fmat)) {
+            dev.new(title = "SAopt: Objective function values")
+            defaults <- list(x    = x$Fmat[,1L],
+                             xlab = "Iteration",
+                             ylab = "Objective function value",
+                             main = "SAopt: Objective function values",
+                             type = "l",
+                             col  = grey(0.6))
+            do.call("plot", defaults, ...)
+
+            defaults$x <- x$Fmat[,2L]
+            defaults$col <- grey(0.2)
+            do.call("lines", defaults, ...)
+
+            defaults$x <- cummin(x$Fmat[,2L])
+            defaults$col <- "blue"
+            do.call("lines", defaults, ...)
+        }
+    } else {
+        .NotYetUsed("plot.type")
+    }
+    invisible()
+}
+
+
