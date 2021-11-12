a9472ac434e4638d8fe54d63d1e6843916386259
Enrico Schumann <es@enricoschumann.net>
2021-11-12 12:18:12 +0100

    [randomReturns] Add argument "exact"
    Add an argument "exact": if TRUE, the created matrix
    has exact means, standard deviations and correlations.
    Default is FALSE.
    
    The case of rho == 1 is now supported.

--- R/randomReturns.R
+++ R/randomReturns.R
@@ -1,12 +1,30 @@
-randomReturns <- function(na, ns, sd, mean = 0, rho = 0) {
+randomReturns <- function(na, ns, sd, mean = 0, rho = 0, exact = FALSE) {
     ## sd   = vol of returns
     ## mean = means of returns
     ##      ==> both may be scalars or vectors of length na
 
-    ans <- rnorm(ns*na)
-    dim(ans) <- c(na, ns)
+    rho1 <- all(rho == 1)
+    rho0 <- identical(rho, 0) || identical(rho, 0L)
 
-    if (!identical(rho, 0)) {
+    if (rho1) {
+        ans <-t(rnorm(ns))[rep(1L, na), , drop = FALSE]
+    } else {
+        ans <- rnorm(ns*na)
+        dim(ans) <- c(na, ns)
+    }
+
+    if (exact) {
+        if (!rho1)
+            ## slightly faster for smaller matrices:
+            ## compute inverse and premultiply
+            ## ans <- t(backsolve(chol(cov(t(ans))), diag(1, na))) %*% ans
+            ans <- solve(t(chol(cov(t(ans)))), ans)
+
+        ans <- ans - rowMeans(ans)
+        ans <- ans / apply(ans, 1, sd)
+    }
+
+    if (!rho0 && !rho1) {
         if (length(rho == 1L)) {
             C <- array(rho, dim = c(na, na))
             diag(C) <- 1
--- inst/tinytest/test_randomReturns.R
+++ inst/tinytest/test_randomReturns.R
@@ -10,3 +10,21 @@ R <- randomReturns(na = 3, ns = 300, sd = 1:3/100,
                    rho = C)
 expect_true(cor(R)[1, 2] > 0.6)
 expect_true(cor(R)[1, 3] < 0.0)
+
+expect_equal(dim(randomReturns(na = 1, ns = 10, sd = 0.02)), c(10, 1))
+
+
+R <- randomReturns(na = 3, ns = 10,
+                   mean = c(0.1, 0.2, 0.3),
+                   sd = 0.02,
+                   exact = TRUE)
+expect_equal(apply(R, 2, sd),   rep(0.02, 3))
+expect_equal(apply(R, 2, mean), c(0.1, 0.2, 0.3))
+expect_equal(cor(R)[lower.tri(cor(R))], rep(0, 3))
+
+R <- randomReturns(na = 3, ns = 10,
+                   mean = c(0.1, 0.2, 0.3),
+                   rho = 0.5,
+                   sd = 0.02,
+                   exact = TRUE)
+expect_equal(cor(R)[lower.tri(cor(R))], rep(0.5, 3))
